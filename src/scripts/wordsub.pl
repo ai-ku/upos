6d545b93dcbd541dece3affcd57c716d424cb163
Deniz Yuret <dyuret@ku.edu.tr>
2012-03-22 12:22:21 +0000

    Cleanup.

--- src/scripts/wordsub.pl
+++ src/scripts/wordsub.pl
@@ -1,30 +1,23 @@
 #!/usr/bin/perl -w
-# Read a substitute file and print word and a random substitute on each line
 use strict;
+
+my $usage = qq{wordsub.pl -n N [-s S] < sub-input
+Reads a substitute file and prints word and a random substitute on each line.
+-n <number of substitutes per word>
+-s <random seed>
+};
+
 use Getopt::Std;
-our($opt_n) = (1);
-getopt('n');
+our($opt_n, $opt_s, $opt_h);
+getopts('n:s:h');
+die $usage if $opt_h;
+$opt_n = 1 unless $opt_n;
+srand($opt_s) if $opt_s;
 
-my @words;
-open(FP, "wsj.test1M.gold");
-while(<FP>) {
-    next unless /\S/;
-    my ($w, $p) = split;
-    # $w =~ tr/A-Z/a-z/;
-    push @words, $w;
-}
-close(FP);
-
-my $index = 0;
-while(<>) {			# wsj.test1M.lc.500.n0p09.subf
+while(<>) {
     my @a = split;
     my $w = $a[0];
     next if $w eq '</s>';
-    # $w =~ tr/A-Z/a-z/;
-    if ($w ne '<unk>' and $w ne $words[$index]) {
-	die "Bad word: [$w] != [$words[$index]]";
-    }
-    $w = $words[$index++];
     my $sum = 0;
     my @sub;
     for (my $i = 1; $i < $#a; $i += 2) {
