c53134d04cc87ea118061560504cc5fa256f6981
Deniz Yuret <dyuret@ku.edu.tr>
2012-03-24 13:46:26 +0000

    Collect and report error output from individual pipeline components.

--- src/scripts/rprun.pl
+++ src/scripts/rprun.pl
@@ -1,14 +1,21 @@
 #!/usr/bin/perl -w
 use strict;
+use File::Temp qw/tempdir/;
 
 my $usage = q{Usage: rprun.pl seed npart ndim Z
-Prints out inputs, output, number of seconds
+Runs random partition experiment with given settings.
+Prints out inputs, output, number of seconds.
 };
 
 my $seed = shift or die $usage;
 my $npart = shift or die $usage;
 my $ndim = shift or die $usage;
 my $Z = shift or die $usage;
+
+my $tmp = tempdir("rprun-XXXX", CLEANUP => 1);
+my $sc_err = "$tmp/scode";
+my $km_err = "$tmp/kmeans";
+my $ev_err = "$tmp/eval";
 
 my $ntest = 1173766;
 my $sc_restart = 1;
@@ -26,12 +33,15 @@ my $scode = "scode -r $sc_restart -i $sc_niter -d $ndim -z $Z -s $seed";
 my $scode2kmeans = "scode2kmeans.pl -t $test";
 my $kmeans = "kmeans --init $km_init --k=$km_k --restarts=$km_restart --seed=$seed";
 my $score = "eval.pl -m -v -g $gold";
-my $cmd = "$input | $rpart | $rpart2scode | $scode | $scode2kmeans | $kmeans | $score |";
 
-my $t = time;
-open(CMD, $cmd) or die $!;
-my $result = <CMD>;
-close(CMD);
-$t = time - $t;
-chomp($result);
-print join("\t", $seed, $npart, $ndim, $Z, $result, $t)."\n";
+my $cmd = "$input | $rpart | $rpart2scode | $scode 2> $sc_err | $scode2kmeans | $kmeans 2> $km_err | $score 2> $ev_err";
+
+my $tm = time;
+system($cmd);
+$tm = time - $tm;
+
+my @sc = split(' ', `cat $sc_err`);
+my @km = split(' ', `cat $km_err`);
+my @ev = split(' ', `cat $ev_err`);
+
+print join("\t", $seed, $npart, $ndim, $Z, @sc, @km, @ev, $tm)."\n";
