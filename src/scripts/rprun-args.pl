5343dda8464fb77e06c0153233274b1fc52c9dbd
Deniz Yuret <dyuret@ku.edu.tr>
2012-03-25 13:26:56 +0000

    *** empty log message ***

--- src/scripts/rprun-args.pl
+++ src/scripts/rprun-args.pl
@@ -5,21 +5,25 @@ use strict;
 my $npart = 65536;
 my $ndim = 25;
 my $Z = 0.166;
+my $seed = 0;
 
 for my $npart (4096, 8192, 16384, 32768, 65536, 92682, 131072, 185364, 262144) {
-    for my $seed (10 .. 19) {
+    for my $s (0 .. 9) {
+	$seed++;
 	print "$seed\t$npart\t$ndim\t$Z\n";
     }
 }
 
 for my $ndim (2, 3, 4, 6, 8, 11, 16, 25, 32, 45, 64, 90, 128, 180, 256) {
-    for my $seed (10 .. 19) {
+    for my $s (0 .. 9) {
+	$seed++;
 	print "$seed\t$npart\t$ndim\t$Z\n";
     }
 }
 
 for my $Z (0.7071, 0.5000, 0.3536, 0.2500, 0.1768, 0.1250, 0.0884, 0.0625, 0.0442) {
-    for my $seed (10 .. 19) {
+    for my $s (0 .. 9) {
+	$seed++;
 	print "$seed\t$npart\t$ndim\t$Z\n";
     }
 }
--- src/scripts/wsrun-args.pl
+++ src/scripts/wsrun-args.pl
@@ -1,8 +1,11 @@
 #!/usr/bin/perl -w
 use strict;
 
+my $seed = 0;
+
 for my $nsub qw(1 2 4 8 16 32 64 90 128 180 256) {
-    for my $seed (10 .. 19) {
+    for my $s (0 .. 9) {
+	$seed++;
 	print "$seed\t$nsub\n";
     }
 }
