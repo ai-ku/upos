71bbeb858381340d07a45090bd7ff75324e749d0
Deniz Yuret <dyuret@ku.edu.tr>
2012-03-27 20:32:05 +0000

    *** empty log message ***

--- src/scripts/kmrun-args.pl
+++ src/scripts/kmrun-args.pl
@@ -6,12 +6,10 @@ use strict;
 my $seed = 0;
 
 for my $file ('rpart.scode.gz', 'wordsub.scode.gz') {
-    for my $nstart (1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024) {
+    for my $nstart (1, 2, 3, 4, 6, 8, 10, 16, 25, 32, 45, 64, 90, 128, 256, 512, 1024) {
 	for my $s (0 .. 9) {
 	    $seed++; print "$seed $file 1 $nstart\n";
-	    if ($nstart <= 64) {
-		$seed++; print "$seed $file 0 $nstart\n";
-	    }
+	    $seed++; print "$seed $file 0 $nstart\n" if $nstart <= 128;
 	}
     }
 }
