2b3b6928240c68a670f483ebe4bcd03303f19981
myatbaz <maliyatbaz@gmail.com>
2012-09-25 20:09:19 +0300

    dist is updated, enc is added for testing. lm ppl vs is tested.

--- src/dist/dists.c
+++ src/dist/dists.c
@@ -10,10 +10,12 @@ char * usage =  "\nUsage: dists [options] < input-stream\n"
 "\t\t2 for Manhattan\n"
 "\t\t3 for Maximum\n"
 "\t\t4 for Jensen\n"
+"\t\t5 for Zero-mean covariance\n"
      "\t-u <upper-bound>\tCalculate 1000NN of the rows up to the <upper-bound>(default number of rows)\n"
      "\t-l <lower-bound>\tCalculate 1000NN of the rows starting from <lower-bound>(default 0)\n"
      "\t-p <arg>\t\tRun <arg> parallel jobs to calculate kNN(default 1)\n"
-     "\t-k <arg>\t\tCalculate <arg>NN of the data(default 1000)\n"
+     "\t-k <arg>\t\tCalculate <arg>NN of the data, set 0 to get all distances.(default 1000)\n"
+     "\t-t Transpose input data"
      "\t-v \t\t\tVerbose\n";
 
 /* NOT TESTED FUNCTIONS
@@ -23,8 +25,9 @@ char * usage =  "\nUsage: dists [options] < input-stream\n"
 int Up;
 int Low;
 int ThreadCount;
-int SubstituteTypes;
+int Ncol;
 int K;
+int Transpose;
 float (* DistFunc)(Row, Row);
 Array Data;
 Hnode ** Dist;
@@ -34,7 +37,8 @@ enum DTYPE {
      cosine = 1,
      manhattan = 2,
      maximum = 3,
-     jensen = 4
+     jensen = 4,
+     zerocov = 5
 } Dtype;
 
 void init_dist(){
@@ -43,11 +47,22 @@ void init_dist(){
      DistFunc = dist_cosine_sparse;
      ThreadCount = 1;
      Up = Low = -1;
-     SubstituteTypes = 0;
+     Ncol = 0;
      K = 1000;
+     Transpose = 0;
      VERBOSE = 0;
 }
-                             
+
+void free_data(Array data){
+     foreach(Row,rr,Data){
+          g_free(rr->val);
+          g_free(rr->lval);
+          g_free(rr->ids);
+          g_free(rr);
+     }      
+     free_array(Data);    
+}
+
 void read_sparse_binary_data_to_array(char * fname){
      FILE * infile;     
      unsigned lr,c, totalByte = 0;
@@ -73,7 +88,7 @@ void read_sparse_binary_data_to_array(char * fname){
           push(Data,rr);
           foreach_int(jj,0,rr->nnz){
                /* This part improves the performance of by pre-calculation */
-               if (rr->ids[jj] > SubstituteTypes) SubstituteTypes = rr->ids[jj];
+               if (rr->ids[jj] > Ncol) Ncol = rr->ids[jj];
                switch(Dtype){
                     case cosine://euclid-precalculation                         
                          rr->lval[jj] = rr->val[jj] * rr->val[jj];
@@ -94,9 +109,9 @@ void read_sparse_binary_data_to_array(char * fname){
           }
      }
      /*Number of unique substitutes in all data*/
-     SubstituteTypes += 1;
+     Ncol += 1;
      fclose(infile);
-     msg("Data[%d x %d = %dBytes]",lr,SubstituteTypes, totalByte);
+     msg("Data[%d x %d = %dBytes]",lr,Ncol, totalByte);
 }
 
 void read_data_stdin(){
@@ -112,7 +127,7 @@ void read_data_stdin(){
                     iter = 0;
                     rr->nnz = nnz;
                     rr->norm = 0;
-                    rr->ids = (unsigned*) calloc(nnz, sizeof(float));
+                    rr->ids = (unsigned*) calloc(nnz, sizeof(unsigned int));
                     rr->val = (float *) calloc(nnz, sizeof(float));
                     if(Dtype == euclid)
                          rr->lval = (float *) calloc(nnz, sizeof(float));
@@ -124,7 +139,7 @@ void read_data_stdin(){
                }
                if (iter % 2 == 0){
                     rr->ids[pos] = atoi(word);
-                    if (rr->ids[pos] > SubstituteTypes) SubstituteTypes = rr->ids[pos];
+                    if (rr->ids[pos] > Ncol) Ncol = rr->ids[pos];
                }
                else if(iter %2 == 1){
                     rr->val[pos] = atof(word);
@@ -134,6 +149,8 @@ void read_data_stdin(){
                          rr->norm += rr->val[pos] * rr->val[pos];
                     else if(Dtype == jensen)
                          rr->lval[pos] = rr->val[pos] * log2(rr->val[pos]);
+                    else if(Dtype == zerocov)
+                         rr->norm += rr->val[pos];
                     pos++;
                }
                iter++;
@@ -143,9 +160,75 @@ void read_data_stdin(){
                foreach_int(i,0,nnz){
                     rr->val[i] /= rr->norm;
                }
+          }      
+     }
+     Ncol += 1;
+     if(Dtype == zerocov){
+          foreach(Row,rr,Data){
+               rr->norm /= Ncol;
           }
      }
-     SubstituteTypes += 1;
+}
+
+void data_transpose(){
+     /*Transposes the input data*/
+     int nrow = length(Data);
+     Array tdata = sized_array(Ncol);
+     int *getnnz = (int*)malloc(Ncol * sizeof(int));
+     msg("data_transpose:[%d %d]\n", nrow, Ncol);
+     foreach_int(ii,0,Ncol)
+          getnnz[ii] = 0;
+     /*Get nonzero values*/
+     msg("Get columnwise element counts");
+     foreach(Row, rr, Data){
+          foreach_int(ii, 0, rr->nnz){
+               getnnz[rr->ids[ii]]++;
+          }
+     }
+     msg("Allocate transpose data");
+     foreach_int(ii,0,Ncol){
+          Row nr = g_new0(struct _Row,1);
+          nr->nnz = 0;
+          nr->norm = 0;
+          nr->ids = (unsigned*) malloc(getnnz[ii] * sizeof(unsigned int));
+          nr->val = (float *) malloc(getnnz[ii] * sizeof(float));
+          push(tdata,nr);
+     }
+     msg("Construct transpose");
+     int ri = 0;
+     foreach(Row, rr, Data){
+          foreach_int(ii, 0, rr->nnz){
+               int cid = rr->ids[ii];
+               Row nr = aref(tdata,cid);
+               int idx = nr->nnz++;
+               nr->ids[idx] = ri;
+               nr->val[idx] = rr->val[ii];
+               nr->norm += rr->val[ii];
+          }
+          ri++;
+     }
+     Ncol = nrow;
+     if(Dtype == zerocov){
+          foreach(Row, nr, tdata){
+               nr->norm /= Ncol;
+          }
+     }
+     else
+          g_error("Transpose currently supported only by zerocov distance metric");
+     free_data(Data);
+     free(getnnz);
+     Data = tdata;
+     /* msg("Print transpose"); */
+     /* ri = 0; */
+     /* foreach(Row, nr, tdata){ */
+     /*      printf("%d", nr->nnz);  */
+     /*      foreach_int(ii, 0, nr->nnz){ */
+     /*           printf(" %d %f", nr->ids[ii], nr->val[ii]); */
+     /*      }  */
+     /*      printf(" norm:%f\n",nr->norm); */
+     /*      if(ri++ > 20) exit(1); */
+     /* }  */
+
 }
 
 void split_thread_work(){
@@ -158,7 +241,7 @@ void split_thread_work(){
      int interval = (r - current_r) / ThreadCount;
      int last = r - current_r - interval * (ThreadCount - 1);
      msg("NumberOfThreads:%d start:%d end:%d",ThreadCount, current_r, r);
-     msg("First n-1 threads will do interval:%d last thread wiil do:%d",interval,last);
+     msg("First n-1 threads will do interval:%d last thread will do:%d",interval,last);
      foreach_int(i,0,ThreadCount){
           if(w[i] == NULL){
                w[i] = (Work)calloc(1,sizeof(struct WorkS));
@@ -181,7 +264,7 @@ void split_thread_work(){
      free(w);
 }
 
-void * dist_sparse_thread(void * ptr){    
+void * dist_sparse_thread(void * ptr){
      Work w = (Work) ptr;
      info(w);
      Hnode n;
@@ -192,7 +275,7 @@ void * dist_sparse_thread(void * ptr){
                fprintf(stderr,".");
           }               
           foreach_int(j, 0, length(Data)){               
-               if (i == j) continue;
+               if (i == j && Dtype != zerocov) continue;
                Row r_j = aref(Data,j);
                n.val = DistFunc(r_i, r_j);
                n.id = j;
@@ -264,6 +347,43 @@ float dist_cosine_sparse(Row p, Row q){
      if (sum > 1) sum = 1;
      else if (sum < -1) sum = -1;
      return acos(sum) * M_1_PI;
+}
+
+float dist_zeromeancov_sparse(Row p, Row q){
+     double sum = 0;
+     int p_i = 0, q_i = 0, pnnz = p->nnz, qnnz = q->nnz, done = 0;
+     unsigned * pids = p->ids, *qids = q->ids;
+     float * pval = p->val, *qval = q->val;
+     while(p_i < pnnz && q_i < qnnz){
+          if (pids[p_i] == qids[q_i]){
+               sum += (pval[p_i] - p->norm) * (qval[q_i] - q->norm);
+               p_i++;
+               q_i++;
+               done++;
+          }
+          else if(pids[p_i] < qids[q_i]){
+               sum += (pval[p_i] - p->norm) * -q->norm;
+               p_i++;
+               done++;
+          }
+          else{
+               sum +=  -p->norm * (qval[q_i] - q->norm);
+               q_i++;
+               done++;
+          }
+     }
+     while(p_i < pnnz){
+          sum += (pval[p_i] - p->norm) * -q->norm;
+          p_i++;
+          done++;
+     }
+     while(q_i < qnnz){
+          sum +=  -p->norm * (qval[q_i] - q->norm);
+          q_i++;
+          done++;
+     }
+     sum += (Ncol - done) * (p->norm * q->norm);     
+     return sum;
 }
 
 float dist_manhattan_sparse(Row p, Row q){
@@ -368,9 +488,9 @@ int main (int argc, char * argv[]){
      int r = -1;
      /*Options of program*/
      int opt;
-     enum DTYPE opt_dist = 0;
+     enum DTYPE opt_dist = 1;
      char *opt_file = NULL;
-     while ((opt = getopt(argc, argv, "d:f:u:l:p:k:hv")) != -1) {
+     while ((opt = getopt(argc, argv, "d:f:u:l:p:k:htv")) != -1) {
           switch(opt) {
                case 'd':
                     opt_dist = atoi(optarg);
@@ -400,6 +520,11 @@ int main (int argc, char * argv[]){
                               Dtype = jensen;
                               DistFunc = dist_jensen_sparse;
                               break;
+                         case zerocov:
+                              msg("Sparse zero-mean covariance");
+                              Dtype = zerocov;
+                              DistFunc = dist_zeromeancov_sparse;
+                              break;
                          default:
                               g_error("Invalid Distance Option");
                     }
@@ -422,6 +547,9 @@ int main (int argc, char * argv[]){
                case 'h':
                     fprintf(stderr,"%s",usage);
                     exit(0);
+               case 't':
+                    Transpose = 1;
+                    break;        
                case 'v':
                     VERBOSE = 1;
                     break;
@@ -431,32 +559,36 @@ int main (int argc, char * argv[]){
           }
      }
      msg("File:%s Distance:%d Threads:%d Up:%d Low:%d KNN:%d", opt_file, opt_dist, ThreadCount, Up, Low, K);
-     if (argc <= 1 || DistFunc == NULL){
-          g_error("%s",usage);
-     }
+     /* if (argc <= 1 || DistFunc == NULL){ */
+     /*      g_error("%s",usage); */
+     /* } */
      start = clock();
-     fprintf(stderr,"read sparse_binary");
-     if (opt_file != NULL)
+     if (opt_file != NULL){
+          msg("read sparse_binary");
           read_sparse_binary_data_to_array(opt_file);          
-     else
-          read_data_stdin();
-     r = length(Data);
-     if(r == 0 || Data == NULL){
-          g_error("Invalid input file");
      }
+     else{
+          msg("read sparse_stdin");
+          read_data_stdin();
+     }
+     if(Transpose)
+          data_transpose();    
+     r = length(Data);     
+     if(r == 0 || Data == NULL)
+          g_error("Invalid input file");
      if (Up == Low){
           Up = r;
           Low = 0;
      }
-     if(Up > -1){
+     if(Up > -1)
           Up = Up > r ? r : Up;
-     }
-     if(Low < -1 || Low >= r){
+     else if(Up == -1)
+          Up = r;     
+     if(Low <= -1 || Low >= r)
           Low = 0;
-     }
      g_assert(Up > Low);
-     K = K > r ? r : K;
-     msg("time:%f",((float)clock() - start) / CLOCKS_PER_SEC);     
+     K = K == 0 ? r : K > r ? r : K;
+     msg("Data[%d %d] read time:%f",length(Data), Ncol, ((float)clock() - start) / CLOCKS_PER_SEC);     
      msg("Allocating Heaps [%d * %dNN]",r , K);
      Dist = (Hnode**)malloc(r * sizeof(Hnode*));
      for(int i = Low; i < Up; i++){
@@ -469,14 +601,10 @@ int main (int argc, char * argv[]){
           printf("%d ",i);
           heap_print(((Hnode**)Dist)[i]);
      }
-     foreach_int(i,0,r){
+     free_data(Data);
+     foreach_int(i,Low,Up){
           g_free(Dist[i]);
-          Row rr = aref(Data, i);               
-          g_free(rr->val);
-          g_free(rr->lval);
-          g_free(rr->ids);
      }    
-     g_free(Dist);
-     free_array(Data);
+     g_free(Dist);     
      return 0;
 }
