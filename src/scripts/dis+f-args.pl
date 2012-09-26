128fdd9694f0d2e9ec50606b0c9906ae79142518
myatbaz <maliyatbaz@gmail.com>
2012-09-26 16:10:26 +0300

    dis and dis+o experiment scripts are added

---
+++ src/scripts/dis+f-args.pl
@@ -0,0 +1,36 @@
+#!/usr/bin/perl -w
+use strict;
+
+my $usage = q{Usage: wsrun-args.pl nclu lang ntok > args
+};
+
+my $seed = 1;
+my $ndim = 25;
+my $nsub = 12;
+my $Z = 0.166;
+my $phi = 50;
+my $nu = 0.2;
+my $K = shift or die $usage;
+my $lang = shift or die $usage;
+my $featfile =  shift or die $usage;
+my $test = "$lang.pos.gz";
+my $line = `zcat $test | wc -l`;
+
+chomp($line);
+for my $nsub qw(1 2 3 4 6 8 10 12 16 25 32 45 64 90 128 180 256) {
+    $seed = 1;
+    for (my $j=0; $j < 10; $j++) {
+	print "$line\t$lang\t$K\t$featfile\t$seed\t$nsub\t$ndim\t$Z\t$phi\t$nu\n";
+	$seed++;
+    }
+}
+
+# for (my $nu=0.1; $nu <= 1; $nu += 0.1) {
+#     for (my $phi=1; $phi <= 100; $phi += 5) {
+# 	$seed = 1;
+# 	for (my $j=0; $j < 10; $j++) {
+# 	    print "$line\t$lang\t$K\t$ntok\t$seed\t$nsub\t$ndim\t$Z\t$phi\t$nu\n";
+# 	    $seed++;
+# 	}
+#     }
+# }
